a0f03aa5dfa9dca93ec4f66fab11b0f8304040f6
Dharanya1990 <b.dharanya.it@gmail.com>
2026-03-02 14:10:35 +0530

    Completed part 3 and partial complete of part 4

--- sales_analysis_tasks.py
+++ sales_analysis_tasks.py
@@ -174,14 +174,198 @@ top_10_cust = sales_df.groupby('customer_id')['total_order_value'].sum().nlarges
 print("Top 10 Customers")
 print(top_10_cust)
 
-# #Part 3 Product Analysis: Best-selling products (by quantity and revenue)
-# top_products = order_items_products_merge_df.groupby('product_name').agg({
-#     'quantity': 'sum',
-#     'item_total_price': 'sum'
-# }).sort_values('item_total_price', ascending=False)
+#Part 3 Product Analysis: Best-selling products (by quantity and revenue)
+product_performance = order_items_products_merge_df.groupby('product_name').agg({
+    'quantity': 'sum',
+    'item_total_price': 'sum'
+}).reset_index()
 
-# # Review Correlation
-# # Assuming reviews joined with products
-# avg_ratings = reviews_df.groupby('product_id')['rating'].mean()
-# prod_stats = products_df.merge(avg_ratings, on='product_id')
-# correlation = prod_stats['price'].corr(prod_stats['rating'])
+#Part 3 Product Analysis: Best-selling products (by quantity)
+top_5_quantity = product_performance.nlargest(5, 'quantity')
+print('Best-selling products (by quantity)')
+print(top_5_quantity)
+
+#Part 3 Product Analysis: Best-selling products (revenue)
+top_5_revenue = product_performance.nlargest(5, 'item_total_price')
+print('Best-selling products (revenue)')
+print(top_5_revenue)
+
+#Part 3 Product Analysis: Worst-performing products
+worst_5_revenue = product_performance.nsmallest(5, 'item_total_price')
+print('Worst-performing products')
+print(worst_5_revenue)
+
+#Part 3 Product Analysis: Average price by category
+category_performance = order_items_products_merge_df.groupby('category').agg({
+    'item_total_price': 'sum'
+}).reset_index()
+
+#Part 3 Product Analysis: Product category generates the most revenue
+topRevenueCategory = category_performance.nlargest(1,'item_total_price')
+print("Product category generates the most revenue")
+print(topRevenueCategory)
+
+#Part 3 Product Analysis: Average price by category
+print("Average price by category")
+average_price_category=order_items_products_merge_df.groupby('category')['item_total_price'].mean()
+print(average_price_category)
+
+#Part 3 Product Analysis: Products with low stock
+print('Products with low stock')
+stock_threshold=10
+lowStockProduct=products_df[products_df['stock_quantity']<stock_threshold][['product_id','product_name','category','stock_quantity']]
+print(lowStockProduct)
+
+#Part 3 Customer Analysis: Customer distribution by state
+print('Customer distribution by state')
+state_dist = customers_df['state'].value_counts()
+top_state = state_dist.idxmax()
+print(state_dist)
+print(f'State has the most customers : {top_state}')
+
+#Part 3 Customer Analysis: New customers per month
+print('New customers per month')
+customers_df['reg_month']=customers_df['registration_date'].dt.to_period('M')
+new_customers_monthly=customers_df['reg_month'].value_counts().reset_index().sort_values(['reg_month'],ascending=True)
+print(new_customers_monthly)
+
+#Part 3 Customer Analysis: Customer retention rate
+purchase_counts = sales_df.groupby('customer_id')['order_id'].nunique()
+repeat_customers = purchase_counts[purchase_counts > 1].count()
+total_customers = customers_df['customer_id'].nunique()
+retention_rate = (repeat_customers / total_customers) * 100
+print(f'Customer retention rate : {retention_rate:.2f}')
+
+#Part 3 Customer Analysis: Average order value by customer segment
+segment_metrics = sales_df.groupby('customer_segment').agg({
+    'total_order_value': 'mean',
+    'CLV': 'mean'
+}).rename(columns={'total_order_value': 'Avg_Order_Value', 'CLV': 'Avg_CLV'})
+print('Average order value by customer segment')
+print(segment_metrics)
+
+#Part 3 Customer Analysis: Percentage of customers are Premium segment
+print('Percentage of customers are Premium segment')
+segment_counts = customers_df['customer_segment'].value_counts(normalize=True) * 100
+premium_pct = segment_counts.get('Premium', 0)
+print(premium_pct)
+
+#Part 3 Customer Analysis: Average customer lifetime value
+total_revenue_per_customer = sales_df.groupby('customer_id')['total_order_value'].sum()
+avg_clv = total_revenue_per_customer.mean()
+print(f"The Average Customer Lifetime Value : {avg_clv:.2f}")
+
+#Part 3 Customer Analysis: Customers made repeat purchases
+print(f"Customers made repeat purchases : {repeat_customers}")
+
+#Part 3 Time Series Analysis: Daily/weekly/monthly sales trends
+monthly_sales = sales_df.set_index('order_date').resample('ME')['total_order_value'].sum()
+print('Monthly sales trends')
+print(monthly_sales)
+
+#Part 3 Time Series Analysis: Seasonal patterns
+sales_df['month_name'] = sales_df['order_date'].dt.month_name()
+seasonal_monthly = sales_df.groupby('month_name')['total_order_value'].sum().reindex([
+    'January', 'February', 'March', 'April', 'May', 'June', 
+    'July', 'August', 'September', 'October', 'November', 'December'
+])
+print('Seasonal patterns')
+print(seasonal_monthly)
+
+#Part 3 Time Series Analysis: Year-over-year growth
+yearly_revenue = sales_df.set_index('order_date').resample('YE')['total_order_value'].sum()
+yoy_growth = yearly_revenue.pct_change() * 100
+
+print("Year-over-year growth")
+for year, rev in yearly_revenue.items():
+    growth = yoy_growth[year]
+    print(f"Year {year.year}: {rev:,.2f} (Growth: {growth:.2f}%)")
+
+#Part 3 Time Series Analysis: Average time between first and second purchase
+ordered_purchases = sales_df.sort_values(['customer_id', 'order_date'])
+first_second_orders = ordered_purchases.groupby('customer_id').head(2)
+first_second_orders['order_rank'] = first_second_orders.groupby('customer_id').cumcount() + 1
+purchase_diff = first_second_orders.pivot(index='customer_id', columns='order_rank', values='order_date')
+if 2 in purchase_diff.columns:
+    days_to_second_order = (purchase_diff[2] - purchase_diff[1]).dt.days.mean()
+    print(f"Average time between first and second purchase: {days_to_second_order:.1f} days")
+
+#Part 3 Time Series Analysis: Payment method is most popular
+popular_payment = sales_df['payment_method'].value_counts().idxmax()
+print(f"Most Popular Payment method: {popular_payment}")
+
+#Part 3 Time Series Analysis: Order cancellation rate
+cancel_rate = (sales_df['status'] == 'Cancelled').mean() * 100
+print(f"Order cancellation rate: {cancel_rate:.2f}%")
+
+#Part 3 Time Series Analysis: Average discount given
+avg_discount = order_items_products_merge_df['discount_percent'].mean()
+print(f"Average Discount: {avg_discount:.2f}%")
+
+#Part 3 Review Analysis: Average rating by product
+review_data = reviews_df.merge(products_df[['product_id', 'product_name', 'category', 'price']], 
+                           on='product_id', 
+                           how='left')
+
+#Part 3 Review Analysis: Average rating by product
+avg_rating_product = review_data.groupby('product_name')['rating'].mean().sort_values(ascending=False)
+print('Average rating by product')
+print(avg_rating_product)
+
+#Part 3 Review Analysis: Average rating by category
+avg_rating_category = review_data.groupby('category')['rating'].mean().sort_values(ascending=False)
+print('Average rating by category')
+print(avg_rating_category)
+
+#Part 3 Review Analysis: Products with most reviews
+most_reviewed = review_data['product_name'].value_counts().head(10)
+print('Products with most reviews')
+print(most_reviewed)
+
+#Part 3 Review Analysis: Correlation between price and rating
+correlation = review_data['price'].corr(review_data['rating'])
+print(f"Correlation between Price and Rating: {correlation:.4f}")
+if correlation > 0.1:
+    print("Insight: Higher priced products tend to have slightly higher ratings.")
+elif correlation < -0.1:
+    print("Insight: Higher priced products tend to have lower ratings (higher expectations).")
+else:
+    print("Insight: There is no significant link between price and rating.")
+
+#Part 4 Cohort Analysis: Group customers by registration month
+customers_df['cohort_month'] = customers_df['registration_date'].dt.to_period('M')
+sales_df = sales_df.merge(customers_df[['customer_id', 'cohort_month']], on='customer_id', how='left')
+sales_df['order_month'] = sales_df['order_date'].dt.to_period('M')
+print('Group customers by registration month')
+print(sales_df[['customer_id','first_name','last_name','order_month']])
+
+#Part 4 Cohort Analysis: Track purchase behavior over time
+sales_df['cohort_index'] = util.get_month_diff(sales_df)
+cohort_pivot = sales_df.pivot_table(index='cohort_month', 
+                                    columns='cohort_index', 
+                                    values='customer_id', 
+                                    aggfunc='nunique')
+print('Track purchase behavior over time')
+print(cohort_pivot)
+
+#Part 4 RFM Analysis: Recency: Days since last purchase
+#Part 4 RFM Analysis: Frequency: Number of orders
+#Part 4 RFM Analysis: Monetary: Total amount spent
+
+snapshot_date = sales_df['order_date'].max() + pd.Timedelta(days=1)
+rfm = sales_df.groupby('customer_id').agg({
+    'order_date': lambda x: (snapshot_date - x.max()).days, # Recency
+    'order_id': 'nunique',                                 # Frequency
+    'total_order_value': 'sum'                                   # Monetary
+}).reset_index()
+rfm.columns = ['customer_id', 'Recency', 'Frequency', 'Monetary']
+rfm['R_Score'] = pd.qcut(rfm['Recency'], 5, labels=[5, 4, 3, 2, 1])
+rfm['F_Score'] = pd.qcut(rfm['Frequency'].rank(method='first'), 5, labels=[1, 2, 3, 4, 5])
+rfm['M_Score'] = pd.qcut(rfm['Monetary'], 5, labels=[1, 2, 3, 4, 5])
+rfm[['R_Score', 'F_Score', 'M_Score']] = rfm[['R_Score', 'F_Score', 'M_Score']].astype(int)
+
+#Part 4 RFM Analysis: Create customer segments based on RFM
+rfm['Segment'] = rfm.apply(util.assign_segment, axis=1)
+segment_counts = rfm['Segment'].value_counts().reset_index()
+print('Customer segments based on RFM (Recency, Frequency, Monetary)')
+print(segment_counts)
--- util.py
+++ util.py
@@ -3,3 +3,26 @@ def removeDuplicate(df):
     print('Duplicates ',dfDuplicateSum)
     if dfDuplicateSum>0:
         df.drop_duplicates(inplace=True)
+
+def get_month_diff(df):
+    # Calculate difference in years and months
+    year_diff = df['order_month'].dt.year - df['cohort_month'].dt.year
+    month_diff = df['order_month'].dt.month - df['cohort_month'].dt.month
+    return year_diff * 12 + month_diff
+
+def assign_segment(df):
+    r = df['R_Score']
+    f = df['F_Score']
+    
+    if r >= 4 and f >= 4:
+        return 'Champions'
+    elif r >= 3 and f >= 3:
+        return 'Loyal Customers'
+    elif r >= 4 and f < 3:
+        return 'New Customers'
+    elif r <= 2 and f >= 4:
+        return 'At Risk / Can\'t Lose'
+    elif r <= 2 and f <= 2:
+        return 'Lost / Hibernating'
+    else:
+        return 'About to Sleep'
